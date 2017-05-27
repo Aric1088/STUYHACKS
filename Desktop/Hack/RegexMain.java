7ad9388c8d0f9812acc043ee35e3b803b0082371
Aric <ariczhuang@gmail.com>
2017-05-27 14:22:46 -0400

    hi lol

--- Desktop/Hack/RegexMain.java
+++ Desktop/Hack/RegexMain.java
@@ -1,7 +1,19 @@
 import java.util.regex.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class RegexMain{
+  
   public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    String input = sc.nextLine();
+    String pattern = "([0-9]+)";
+    Pattern p = Pattern.compile(pattern);
+    Matcher m = p.matcher(input);
+    while (m.find()){
+      System.out.println(m.group());
+    }
+    
   }
+ 
 }
